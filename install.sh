4c2b02f0b3cc29ba1e7ac2d71258f3365a275854
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-01 13:58:08 +0300

    Simplify install script

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -e
-if (( $EUID != 0 )); then
-    echo "Please run via sudo"
-    exit
-fi
-# Install dependencies
 apt install \
     git \
     libusb-1.0-0-dev \
@@ -12,9 +7,7 @@ apt install \
     qt5-qmake \
     qtbase5-dev-tools \
     g++
-# Run build script
 cd scripts
 ./build_all.sh
-# Add permissions for Pixy USB interface
 cd ../src/host/linux/
 cp pixy.rules /etc/udev/rules.d/
