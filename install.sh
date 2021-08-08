d04c280bd3aa63828b05a96fa342feca5f217c0f
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-08 14:46:11 +0300

    Improve install script

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -e
-apt install \
+sudo apt install \
     git \
     libusb-1.0-0-dev \
     qt5-default \
     qt5-qmake \
     qtbase5-dev-tools \
     g++
+sudo chown -R $USER .
+PRIMARY_GROUP=$(id -gn $USER)
+sudo chgrp -R $PRIMARY_GROUP .
 cd scripts
 ./build_all.sh
 cd ../src/host/linux/
-cp pixy.rules /etc/udev/rules.d/
+sudo cp pixy.rules /etc/udev/rules.d/
